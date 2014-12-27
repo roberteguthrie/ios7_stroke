400d22f55eee49526bb2a0495673332d5d9727cb
roberteguthrie <guthrierb1@gmail.com>
2014-12-27 13:07:03 -0600

    commit changes
    commit changes 2014 1227 01067PM

--- ios7_stroke/ScrollView1ViewController.m
+++ ios7_stroke/ScrollView1ViewController.m
@@ -9,6 +9,7 @@
 #import "ScrollView1ViewController.h"
 
 @interface ScrollView1ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *ButtonOutlet;
 
 @end
 
@@ -18,12 +19,23 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    NSMutableAttributedString *title =
+        [[NSMutableAttributedString  alloc] initWithString:self.ButtonOutlet.currentTitle];
+    
+    
+    
+#if 0
+    [title setAttributes:@{  NSStrokeWidthAttributeName : @3,
+                             NSStrokeColorAttributeName : self.ButtonOutlet.tintColor }
+                   range:NSMakeRange(0, [title length])];
+#endif
+    
+    [title setAttributes:@{  NSStrokeWidthAttributeName : @3 }
+                   range:NSMakeRange(0, [title length])];
+ 
+    
+    
+    [self.ButtonOutlet setAttributedTitle:title forState:UIControlStateNormal];
 }
 
 @end
